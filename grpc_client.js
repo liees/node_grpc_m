aeba1390f7b40127b31f33c32acac471a1018268
liees.liang <liees.liang@pimax.com>
2022-10-31 16:43:10 +0800

    use @grpc/grpc-js

--- grpc_client.js
+++ grpc_client.js
@@ -1,19 +1,29 @@
 
-const grpc = require('grpc');
-const Promise = require('bluebird');
-const PROTO_PATH = __dirname + '/server.proto';
-const user_proto = grpc.load(PROTO_PATH).userproto;
-const UserGrpcClient = new user_proto.User('localhost:50051', grpc.credentials.createInsecure());
+const grpc  = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
 
+const PROTO_PATH = __dirname + '/server.proto';
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const proto  = grpc.loadPackageDefinition(packageDefinition);
+
+console.log(proto)
+
+const UserGrpcClient = new proto.userproto.User('localhost:50051', grpc.credentials.createInsecure());
+
+console.log('============================')
+console.log(UserGrpcClient)
+console.log('============================')
 Promise.promisifyAll(UserGrpcClient);
 
 let userId = '76543'
 let condition = JSON.stringify({isDeleted: false});
 
-UserGrpcClient.getById(userId, (err, result) => {
+UserGrpcClient.getById({ userId }, (err, result) => {
   if(!!err) {
     return print(err);
   }
+  console.log(result)
   return print(result.message);
 })
 
--- grpc_service.js
+++ grpc_service.js
@@ -1,27 +1,30 @@
-const grpc = require('grpc');
-const path = require('path');
-const Promise = require('bluebird')
+const grpc  = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
 const PROTO_PATH = __dirname + '/server.proto';
-const user_proto = grpc.load(PROTO_PATH).userproto;
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const proto  = grpc.loadPackageDefinition(packageDefinition);
 
 let getById = (call, callback) => {
+  console.log(call)
   let userId = call.request.userId;
-  callback(null, 'findById(' + userId + ')');
+  callback(null, { message: 'findById(' + userId + ')'});
 }
 
 let getList = async (call, callback) => {
   let condition = call.request.condition;
-  callback(null, 'find(' + condition + ')');
+  callback(null, { message: 'find(' + condition + ')'});
 }
 
 const main  = async () => {
   const server = new grpc.Server();
-  server.addService(user_proto.User.service, {
-    getById: getById,
-    getList: getList,
+  server.addService(proto.userproto.User.service, {
+    getById,
+    getList,
   });
-  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-  server.start();
+  server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+  });
+
 };
 
 main();
